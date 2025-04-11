deb0a6a883af91e6db353b7bb3c7b4c5242fd5e5
dochmai382 <dochmai382@gmail.com>
2025-04-11 23:22:50 +0900

    initial commit

---
+++ src/main/java/org/example/llmquestionboard/LlmQuestionBoardApplication.java
@@ -0,0 +1,13 @@
+package org.example.llmquestionboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmQuestionBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmQuestionBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/llmquestionboard/LlmQuestionBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.llmquestionboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmQuestionBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
