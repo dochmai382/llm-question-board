5bc65d2a685a768668e2d6da92e488692c99edbb
dochmai382 <dochmai382@gmail.com>
2025-04-12 23:16:41 +0900

    feat: 유저 ID를 UUID로 변경

--- src/main/java/org/example/llmquestionboard/model/security/CustomUserDetails.java
+++ src/main/java/org/example/llmquestionboard/model/security/CustomUserDetails.java
@@ -6,14 +6,20 @@ import org.springframework.security.core.userdetails.User;
 import java.util.Collection;
 
 public class CustomUserDetails extends User {
+    private final String userId;
     private final String nickname;  // 추가된 닉네임 필드
 
-    public CustomUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities, String nickname) {
+    public CustomUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities, String nickname, String userId) {
         super(username, password, authorities);
         this.nickname = nickname;
+        this.userId = userId;
     }
 
     public String getNickname() {
         return nickname;
+    }
+
+    public String getUserId() {
+        return userId;
     }
 }
